414c1f460f9556f051dca0c541b208833d98ff15
lazberry <lazberry89@gmail.com>
2026-07-24 02:02:39 +0900

    feat: add @ConfigObject O/R mapping and @Virtual scanning filter - @Async, @Sync for thread controlling. - Implement ConfigObjectMapper to automatically bind YAML sections to POJOs/Records - Register @ConfigObject instances directly into DependencyContainer upon boot - Introduce @Virtual annotation to exclude template/abstract components from package scanning - Update PackageScanner pipeline to handle @Virtual and @ConfigObject - Fix ByteBuddy dependency scope in Gradle from testImplementation to implementation

--- lrf-common/src/main/java/Framework/Annotation/Async.java
+++ lrf-common/src/main/java/Framework/Annotation/Async.java
@@ -1,5 +1,6 @@
 package Framework.Annotation;
 
+import javax.management.monitor.Monitor;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
